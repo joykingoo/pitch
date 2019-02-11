e9ff33cd917751576cf9d11e19cabcb527cc3be5
joy <wendokingoo@gmail.com>
2019-02-11 21:43:14 +0300

    forms and views in main folder modified

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,10 +3,15 @@ from wtforms import StringField,TextAreaField,SubmitField,ValidationError
 from wtforms.validators import Required,Email
 #from .models import User
 
-class ReviewForm(FlaskForm):
+class PitchForm(FlaskForm):
+    title = StringField('Pitch Form', validators=[Required()])
+    pitch = TextAreaField('Add Pitch',validators=[Required()])
+
+
+class CommentForm(FlaskForm):
 
     title = StringField('Review title',validators=[Required()])
-    review = TextAreaField('Movie review', validators=[Required()])
+    comment = TextAreaField('Pitch review', validators=[Required()])
     submit = SubmitField('Submit')
 
 class UpdateProfile(FlaskForm):
